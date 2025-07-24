9660ee2135accfefbcd505d3d81a4e2415036ad3
Jd Rodriguez <136628670+jdrodriguezrodriguez@users.noreply.github.com>
2025-07-24 07:05:28 +0000

    style: implementar formulario y diseño para actualizar usuario

--- src/main/resources/public/js/script.js
+++ src/main/resources/public/js/script.js
@@ -2,9 +2,39 @@ window.onload = function() {
     console.log("Página cargada con éxito.");
 };
 
-
+/*NAV*/
 const nav = document.querySelector('.nav');
 
     window.addEventListener('scroll', function(){
         nav.classList.toggle('active', window.scrollY >0)
     })
+
+
+/*OVERLAY - UPDATE*/
+document.getElementById("link-trigger-update").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    document.getElementById("overlay-update").style.display = "block"; 
+    document.getElementById("menu-update").style.display = "block"; 
+});
+
+
+
+document.getElementById("overlay-update").addEventListener("click", function() {
+    document.getElementById("overlay-update").style.display = "none"; 
+    document.getElementById("menu-update").style.display = "none"; 
+});
+
+/*
+//OVERLAY PARA ACCESO
+document.getElementById("menu-trigger").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    document.getElementById("overlay").style.display = "block"; 
+    document.getElementById("menu").style.display = "block"; 
+});
+
+
+document.getElementById("overlay").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "none"; 
+    document.getElementById("menu").style.display = "none"; 
+});
+ */
